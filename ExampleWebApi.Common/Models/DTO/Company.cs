48a838e0747823debef163cd8ad6225f07707389
OVB <127322769+ovb006@users.noreply.github.com>
2024-09-01 23:57:34 -0400

    Update Company.cs
    Removing needless comment

--- ExampleWebApi.Common/Models/DTO/Company.cs
+++ ExampleWebApi.Common/Models/DTO/Company.cs
@@ -1,12 +1,5 @@
 ﻿namespace ExampleWebApi.Common.Models.DTO
 {
-    //Why did I not take the lazy way out and use the public DTOs as my data model?
-    //Two reasons - 1 it's inefficient to structure the data that way, it's overly normalized
-    // 2, I wanted to demonstrate how I'd add convenience methods to the DTOs to make them easier to work with
-    // I realize the argument could be made here that this couples the api to the data layer.
-    // I'd argue that the DTOs are a contract between the api and the client, and that the api should be the one to define that contract.
-    // but that internally data should be structured in the way that is most efficient.
-
     public class Company : CompanyHeader
     {
         public EmployeeHeader[] Employees { get; set; }
